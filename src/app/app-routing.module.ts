87bb3fb3900f0bf411e550ca7f96deb2394dc50e
Stanley Nwosu <stanleenwosu@yahoo.com>
2022-05-19 20:44:09 +0100

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo: 'home',
+    pathMatch:'full'
+  },
+  {
+    path:'home',
+    component: HomeComponent
+  },
+  {
+    path:'about',
+    component: AboutComponent
+  },
+  {
+    path:'contact',
+    component: ContactComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
